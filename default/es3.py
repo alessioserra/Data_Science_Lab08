2c6536a1456314137059d33a78e333b840fa63da
zierp <zierp@LAPTOP-4TV42EQM>
2019-11-28 15:38:06 +0100

    Lab finished

--- default/es3.py
+++ default/es3.py
@@ -6,20 +6,13 @@ Created on 28 nov 2019
 import csv
 import matplotlib.pyplot as plot
 import numpy as np
-from IPython.utils.py3compat import xrange
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics.classification import classification_report
 from sklearn.ensemble.forest import RandomForestRegressor
 from sklearn.metrics.regression import r2_score
-
-def rolling_window(seq, window_size):
-    it = iter(seq)
-    win = [it.next() for cnt in xrange(window_size)] # First window
-    yield win
-    for e in it: # Subsequent windows
-        win[:-1] = win[1:]
-        win[-1] = e
-        yield win
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR 
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
 dataset = []
 
@@ -70,45 +63,79 @@ plot.xlabel("Date")
 plot.ylabel("Temperature Celsius")
 plot.show()
 
-# Exercise 5
-T = len(X)
-W=9 # suggested by Nick
-forecast = []
-days_to_predict=3
-"""..."""
-
-
-# Exercise 6
+# Exercise 5-6-7
 """Date from 1940 to 1944"""
 data_train = []
-y_train = []
+means_train = []
 for el,yy in zip(X,y):
     if np.datetime64(el) <= np.datetime64('1944-12-31'):
         data_train.append(el)
-        y_train.append(yy)
-    
+        means_train.append(yy)
 
 """Date in 1945"""
 data_test = []
-y_test = []
+means_test = []
 for el,yy in zip(X,y):
     if np.datetime64(el) > np.datetime64('1944-12-31'):
         data_test.append(el)
-        y_test.append(yy)
+        means_test.append(yy)
 
-"""Convert date"""
-d_train = []
-for i in enumerate(data_train):
-    d_train.append(i[0])
-    
-d_test = []
-for i in enumerate(data_test):
-    d_test.append(i[0]) 
-    
-d_train = np.array(d_train)
-d_test = np.array(d_test)
+"""WINDOW"""
+l_train = len(data_train)
+windows_train = []
+y_train = []
+W=9 # suggested by Nick
+days_to_predict=2
 
-reg = RandomForestRegressor(n_estimators=100)
-reg.fit(d_train.reshape(-1,1), y_train)
-y_pred = reg.predict(d_test.reshape(-1,1))
-print("Accuracy : ",r2_score(y_pred, y_test)) #Accuracy
+"""Windows train"""
+for i in range(l_train-2*W-days_to_predict):
+    windows_train.append(means_train[i:i+W+1])
+    y_train.append(means_train[i+W+days_to_predict])
+
+l_test = len(data_test)
+windows_test = []
+y_test = []
+
+"""Windows test"""
+for i in range(l_test-2*W-days_to_predict):
+    windows_test.append(means_test[i:i+W+1])
+    y_test.append(means_test[i+W+days_to_predict])
+   
+"""Convert type"""
+windows_train = np.array(windows_train).astype('float32')
+windows_test = np.array(windows_test).astype('float32')
+y_train = np.array(y_train).astype('float32')
+y_test = np.array(y_test).astype('float32')
+    
+"""Random Forest"""
+reg = RandomForestRegressor(1000,n_jobs=-1)
+reg.fit(windows_train,y_train)
+y_pred = reg.predict(windows_test)
+print('R2 score of RandomForestRegressor:',r2_score(y_test,y_pred))
+
+"""Lasso"""
+reg2 = make_pipeline(PolynomialFeatures(5), Lasso(alpha=0.5,fit_intercept=True,max_iter=1000))
+reg2.fit(windows_train,y_train)
+y_pred2 = reg2.predict(windows_test)
+print('R2 score of Lasso:',r2_score(y_test,y_pred2))
+
+"""Ridge"""
+reg3 = make_pipeline(PolynomialFeatures(3), Ridge(alpha=0.5,fit_intercept=True))
+reg3.fit(windows_train,y_train)
+y_pred3 = reg3.predict(windows_test)
+print('R2 score of Ridge:',r2_score(y_test,y_pred3))
+
+"""SVR"""
+reg4 = SVR(gamma='scale')
+reg4.fit(windows_train,y_train)
+y_pred4 = reg4.predict(windows_test)
+print('R2 score of SVR:',r2_score(y_test,y_pred4))
+
+
+"""Final plot"""
+if plot:
+    fig = plot.figure()  
+    plot.plot(np.arange(0,len(y_test)), y_test, c='red')
+    #plt.plot(np.arange(0,len(y_test)), y_pred2, c='red')
+    plot.plot(np.arange(0,len(y_test)), y_pred4, c='blue')
+    plot.show()
